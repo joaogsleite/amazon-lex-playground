af8f0e998ea19b220617e6dcbc4055bfc3a5a9a5
joaogsleite <joao.leite@isobar.com>
2020-02-28 17:19:04 +0000

    added DialogLambda

--- lambdas/OrderFlowersDialogLambda/index.js
+++ lambdas/OrderFlowersDialogLambda/index.js
@@ -1,14 +1,12 @@
-exports.handler = function (event, context, callback) {
-  const response = {
-    sessionAttributes: event.sessionAttributes,
-    dialogAction: {
-      type: 'Close',
-      fulfillmentState: 'Fulfilled',
-      message: { 
-        contentType: 'PlainText',
-        content: `Example response`,
-      },
-    },
-  };
-  callback(response);
+const { delegate } = require('./dialogActions');
+
+function handleMessage(intent) {
+  return delegate(intent);
 }
+
+exports.handler = async function (event) {
+  const response = await handleMessage(event.currentIntent);
+  return {
+    dialogAction: response,
+  };
+};
